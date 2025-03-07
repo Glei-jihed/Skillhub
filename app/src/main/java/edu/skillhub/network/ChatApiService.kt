f0faf30b8eacc665de57729733305052bf47fe83
Glei-jihed <glei.jihed@gmail.com>
2025-03-07 11:45:22 +0100

    retrofit config update

--- app/src/main/java/edu/skillhub/network/ChatApiService.kt
+++ app/src/main/java/edu/skillhub/network/ChatApiService.kt
@@ -1,9 +1,11 @@
 package edu.skillhub.network
 
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ChatApiService {
-    @POST("/")
+    @Headers("Content-Type: application/json")
+    @POST("chatbot/chatbot")
     suspend fun sendMessage(@Body request: ChatRequest): ChatResponse
 }
--- app/src/main/java/edu/skillhub/network/RetrofitClient.kt
+++ app/src/main/java/edu/skillhub/network/RetrofitClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://votre.api.base.url/"
+
+    private const val BASE_URL = "http://2080-94-228-190-38.ngrok-free.app/"
 
     val apiService: ChatApiService by lazy {
         Retrofit.Builder()
